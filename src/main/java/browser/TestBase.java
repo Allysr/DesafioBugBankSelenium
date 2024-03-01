4e7dabe49d9408b462dc6b55624b2a8e623341b7
alicia souza <alicia.rsouza@gmail.com>
2024-03-01 18:17:11 -0300

    feat: Configurações iniciais

---
+++ src/main/java/browser/TestBase.java
@@ -0,0 +1,25 @@
+package browser;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+
+public class TestBase {
+    private static WebDriver driver;
+
+    public static WebDriver getDriver(){
+        driver = DriverFactory.getOrCreateDriver(Browser.CHROME);
+        return driver;
+    }
+    @BeforeEach
+    public void setupDriver() {
+        driver.get("https://bugbank.netlify.app/#");
+    }
+
+    @AfterEach
+    public void teardown() {
+        if (driver != null) {
+            DriverFactory.quitDriver();
+        }
+    }
+}
---
+++ src/main/java/browser/Waits.java
@@ -0,0 +1,31 @@
+package browser;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Waits {
+    private WebDriver driver;
+
+    public Waits(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public WebElement visibilityOfElement(By by){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+
+    public  WebElement loadElement(WebElement elemento){
+        try{
+            return new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.visibilityOf(elemento));
+        }catch (WebDriverException e){
+            return elemento;
+        }
+    }
+}
